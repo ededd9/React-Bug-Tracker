1f4b2c7417fc1a6c71a2c6564cf779ba2a348e4f
Edward Hurtado <edhur9@gmail.com>
2022-04-14 14:50:43 -0400

    created mainpage jsx

---
+++ src/pages/mainPage/mainPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./mainPage.css";
+import SideBar from "../../components/sideBar/sideBar";
+function mainPage() {
+  return (
+    <>
+      <div className="mainpage-container">
+        testing
+        <SideBar />
+      </div>
+    </>
+  );
+}
+
+export default mainPage;
