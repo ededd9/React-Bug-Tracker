489e37fac47dd2f5aa06c6a64a2888a1aa03c81e
Edward Hurtado <edhur9@gmail.com>
2022-04-24 00:16:00 -0400

    added capitalize function and ternary chain to color code severity

--- src/components/viewTickets/viewTickets.jsx
+++ src/components/viewTickets/viewTickets.jsx
@@ -2,8 +2,12 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./viewTickets.css";
 function ViewTickets() {
+  // VARIABLES
   const [tickets, setTickets] = useState([]);
-
+  //FUNCTIONS
+  const capitalize = (str) => {
+    return str[0].toUpperCase() + str.substring(1);
+  };
   useEffect(() => {
     const displayTickets = async () => {
       const res = await axios.get("http://localhost:3000/");
@@ -31,7 +35,21 @@ function ViewTickets() {
               <>
                 <td>{ticket.department}</td>
                 <td>{ticket.subject}</td>
-                <td>{ticket.severity}</td>
+                {/* simple ternary chain to colorcode severity status */}
+                {ticket.severity === "low" ? (
+                  <td style={{ color: "lightgreen" }}>
+                    {capitalize(ticket.severity)}
+                  </td>
+                ) : ticket.severity === "medium" ? (
+                  <td style={{ color: "orange" }}>
+                    {capitalize(ticket.severity)}
+                  </td>
+                ) : (
+                  <td style={{ color: "red" }}>
+                    {capitalize(ticket.severity)}
+                  </td>
+                )}
+
                 <td>{ticket.description}</td>
                 <td>{new Date(ticket.createdAt).toLocaleDateString()}</td>
               </>
