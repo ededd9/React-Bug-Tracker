fde1b04f58a926464a64294e1b351acda5a705c6
Edward Hurtado <edhur9@gmail.com>
2022-04-30 00:18:14 -0400

    added link to dashboard

--- src/components/sideBar/sideBar.jsx
+++ src/components/sideBar/sideBar.jsx
@@ -1,32 +1,43 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./sideBar.css";
 function sideBar() {
   return (
-    <div className="sideBar-container">
-      <div className="logo-container">
-        <div className="name-of-logo">Bug Tracker</div>
-        <i className="logo-icon fa-solid fa-layer-group"></i>
+    <>
+      <div className="sideBar-container">
+        <div className="logo-container">
+          <div className="name-of-logo">Ticket System</div>
+          <i className="logo-icon fa-solid fa-layer-group"></i>
+        </div>
+        <ul className="list-container">
+          <li>
+            <Link to="/home" className="link">
+              <i className="list-icons fa-solid fa-house"></i>
+              Homepage
+            </Link>
+          </li>
+          <li>
+            <Link to="/dashboard" className="link">
+              <i className="list-icons fa-solid fa-chart-simple"></i>Dashboard
+            </Link>
+          </li>
+          <li>
+            <Link to="/createTicket" className="link">
+              <i className="list-icons fa-solid fa-plus"></i>Add Ticket
+            </Link>
+          </li>
+          <li>
+            <i className="list-icons fa-solid fa-user"></i>Profile
+          </li>
+          <li>
+            <Link to="/login" className="link">
+              <i className="list-icons fa-solid fa-arrow-right-from-bracket"></i>
+              Logout
+            </Link>
+          </li>
+        </ul>
       </div>
-      <ul className="list-container">
-        <li>
-          <i className="list-icons fa-solid fa-house"></i>
-          Homepage
-        </li>
-        <li>
-          <i className="list-icons fa-solid fa-chart-simple"></i>Statistics
-        </li>
-        <li>
-          <i className="list-icons fa-solid fa-plus"></i>Add Ticket
-        </li>
-        <li>
-          <i className="list-icons fa-solid fa-user"></i>Profile
-        </li>
-        <li>
-          <i className="list-icons fa-solid fa-arrow-right-from-bracket"></i>
-          Logout
-        </li>
-      </ul>
-    </div>
+    </>
   );
 }
 
