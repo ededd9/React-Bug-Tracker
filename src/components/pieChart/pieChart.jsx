773420515b4d2385a8d5a7cf5dcece350b4e9ede
Edward Hurtado <edhur9@gmail.com>
2022-04-30 00:18:59 -0400

    created a pie chart component to help analyze data based on severity

---
+++ src/components/pieChart/pieChart.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { useEffect } from "react";
+import * as d3 from "d3";
+function PieChart(props) {
+  const { data, outerRadius, innerRadius } = props;
+  const margin = { top: 50, right: 50, bottom: 50, left: 50 };
+  const width = 2 * outerRadius + margin.left + margin.right;
+  const height = 2 * outerRadius + margin.top + margin.bottom;
+  useEffect(() => {
+    drawChart();
+  }, [data]);
+  function drawChart() {
+    const colorScale = d3
+      .scaleSequential()
+      .interpolator(d3.interpolateCool)
+      .domain([0, data.length]);
+    d3.select("#pie-container").select("svg").remove();
+    const svg = d3
+      .select("#pie-container")
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .append("g")
+      .attr("transform", `translate(${width / 2}, ${height / 2})`);
+    const arcGenerator = d3
+      .arc()
+      .innerRadius(innerRadius)
+      .outerRadius(outerRadius);
+    const pieGenerator = d3
+      .pie()
+      .padAngle(0)
+      .value((d) => d.value);
+    const arc = svg.selectAll().data(pieGenerator(data)).enter();
+    arc
+      .append("path")
+      .attr("d", arcGenerator)
+      .style("fill", (_, i) => d3.scaleLinear(i))
+      .style("stroke", "#ffffff")
+      .style("stroke-width", 0);
+    arc
+      .append("text")
+      .attr("text-anchor", "middle")
+      .attr("alignment-baseline", "middle")
+      .text((d) => [d.data.label, d.data.value])
+      .style("fill", "#ffffff")
+      .attr("transform", (d) => {
+        const [x, y] = arcGenerator.centroid(d);
+        return `translate(${x}, ${y})`;
+      });
+  }
+  return <div id="pie-container"></div>;
+}
+
+export default PieChart;
